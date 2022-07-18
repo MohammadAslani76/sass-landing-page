73da1233c89c3b0d1cfd61198cc34676935ec467
MohammadAslani76 <mohammadaslani001@gmail.com>
2022-07-18 23:09:25 +0430

    create gitignore file

--- js/app.js
+++ js/app.js
@@ -3,7 +3,11 @@ const closeMenu = document.querySelector("#closeMenu");
 const menu = document.querySelector("#menu");
 openMenu.addEventListener("click" , () => {
     menu.style.display= 'block';
+    menu.classList.add('show');
+    menu.classList.remove('hide');
 })
 closeMenu.addEventListener("click" , () => {
+    menu.classList.add('hide');
+    menu.classList.remove('show');
     menu.style.display= "none";
 })
